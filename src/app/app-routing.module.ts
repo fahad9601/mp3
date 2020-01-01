b820e9324a32e5b6e5acaae5027f48068081303f
fahad9601 <fahad9601@gmail.com>
2020-01-01 18:35:25 +0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FaqComponent } from './faq/faq.component';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { TermsComponent } from './terms/terms.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { CopyrightComponent } from './copyright/copyright.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'news', component: NewsComponent},
+  {path: 'faq', component: FaqComponent},
+  {path: 'privacy', component: PrivacyComponent},
+  {path: 'copyright', component: CopyrightComponent},
+  {path: 'terms', component: TermsComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatInputModule,
+   MatButtonModule,
+   MatToolbarModule,
+    MatExpansionModule
+  } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { FaqComponent } from './faq/faq.component';
+import { NewsComponent } from './news/news.component';
+import { HeaderComponent } from './header/header.component';
+import { TermsComponent } from './terms/terms.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { CopyrightComponent } from './copyright/copyright.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FaqComponent,
+    NewsComponent,
+    HeaderComponent,
+    TermsComponent,
+    PrivacyComponent,
+    CopyrightComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+     MatExpansionModule,
+     MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
